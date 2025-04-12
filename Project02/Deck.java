82860ba7472215fd04133002d2fa725eca2dbc54
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2025-04-12 17:49:50 -0500

    Update Deck.java

--- Project02/Deck.java
+++ Project02/Deck.java
@@ -1,3 +1,42 @@
-public class Deck {
+public final class Deck {
+    private Card[] deck;
+    private int dealt;
+
+    public Deck() {
+        deck = new Card[52];
+        int index = 0;
+        for (int suit = 0; suit < 4; suit++) {
+            for (int rank = 1; rank <= 13; rank++) {
+                deck[index] = new Card(rank, suit);
+                index++;
+            }
+        }
+        dealt = -1;
+        shuffle();
+    }
     
+    public void shuffle() {
+        for (int i = 0; i < deck.length; i++) {
+            int j = (int) (Math.random() * deck.length);
+            Card temp = deck[i];
+            deck[i] = deck[j];
+            deck[j] = temp;
+        }
+    }
+
+    public Card draw() {
+        if (dealt < deck.length) {
+            return deck[dealt++];
+        } else {
+            return null; // No more cards to draw
+        }
+    }
+
+    public int cardsRemaining() {
+        return deck.length - dealt - 1;
+    }
+
+    public boolean isEmpty() {
+        return dealt >= deck.length - 1;
+    }
 }
