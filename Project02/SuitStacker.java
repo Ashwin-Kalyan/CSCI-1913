d281f9f6a25dfd26d3c27e36304f046f1cfd5a7d
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2025-04-13 17:03:39 -0500

    Update SuitStacker.java

--- Project02/SuitStacker.java
+++ Project02/SuitStacker.java
@@ -1,3 +1,6 @@
-public class SuitStacker {
-    
+public class SuitStacker implements Player {
+    @Override
+    public int nextMove(Card card, Board board) {
+        return card.getSuitNum() -1;
+    }
 }
