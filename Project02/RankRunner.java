eeb2b1058856ef63cec3f4194e0136ac87f3a803
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2025-04-13 17:00:53 -0500

    Update RankRunner.java

--- Project02/RankRunner.java
+++ Project02/RankRunner.java
@@ -1,3 +1,6 @@
-public class RankRunner {
-    
+public class RankRunner implements Player {
+    @Override
+    public int nextMove(Card card, Board board) {
+        return (card.getRankNum() - 1) % 4;
+    }
 }
