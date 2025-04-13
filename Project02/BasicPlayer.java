5473000716f4ef3eb30c5f87a1b55c53be442d6f
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2025-04-13 16:56:39 -0500

    Update BasicPlayer.java

--- Project02/BasicPlayer.java
+++ Project02/BasicPlayer.java
@@ -1,3 +1,9 @@
-public class BasicPlayer {
-    
+public class BasicPlayer implements Player {
+    @Override
+    public int nextMove(Card card, Board board) {
+        for (int col = 0; col < 4; col++) {
+            if (board.canPlay(card, col)) return col;
+        }
+        return -1;
+    }
 }
