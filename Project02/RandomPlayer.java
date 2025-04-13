a08c9ad35c6dcfe720fc40046463971a3b08a115
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2025-04-13 16:59:18 -0500

    Update RandomPlayer.java

--- Project02/RandomPlayer.java
+++ Project02/RandomPlayer.java
@@ -1,3 +1,14 @@
-public class RandomPlayer {
-    
+import java.util.Random;
+
+public class RandomPlayer implements Player {
+    private Random random;
+
+    public RandomPlayer() {
+        this.random = new Random();
+    }
+
+    @Override
+    public int nextMove(Card card, Board board) {
+        return random.nextInt(4);
+    }
 }
