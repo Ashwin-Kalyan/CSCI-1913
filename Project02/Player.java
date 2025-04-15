94fd9b2dc76776436a03d8a6cf561465ac97c1e0
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2025-04-14 19:06:47 -0500

    player docs and comments

--- Project02/Player.java
+++ Project02/Player.java
@@ -1,3 +1,16 @@
-public interface  Player {
+// CSCI 1913 - Project 2
+// @author: Ashwin Kalyan
+
+/**
+ * This interface defines the Player class.
+ * It contains a method to get the next move of the player.
+ */
+public interface Player {
+    /**
+     * This method returns the next move of the player.
+     * @param card - the card that the player has in hand
+     * @param board - the board on which the game is being played
+     * @return - the next move of the player
+     */
     int nextMove(Card card, Board board);
 }
