4932068d6e0d3181570154e60085c2c919cbbe88
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2025-04-14 19:59:33 -0500

    as

--- Project02/PlayerComparison.java
+++ Project02/PlayerComparison.java
@@ -1,6 +1,17 @@
-public class PlayerComparison {
-    private static final int NUM_GAMES = 10000;
+// CSCI 1913 - Project 2
+// @author: Ashwin Kalyan
 
+/**
+ * This class compares the performance of different AI players in the ConnectFwar game.
+ */
+public class PlayerComparison {
+    private static final int NUM_GAMES = 10000; // 10K rounds played by each AI
+
+    /**
+     * Main method to run the simulation and compare players.
+     * It creates instances of different player strategies and runs the game simulation.
+     * The average score for each player is calculated and printed.
+     */
     public static void main(String[] args) {
         Player randomPlayer = new RandomPlayer();
         Player basicPlayer = new BasicPlayer();
@@ -20,6 +31,11 @@ public class PlayerComparison {
         System.out.printf("ranks %.4f\n", rankAvg);
     }
 
+    /**
+     * Runs a simulation of the ConnectFwar game for a given player.
+     * @param player - The player strategy to simulate.
+     * @return - The average score of the player after NUM_GAMES rounds.
+     */
     private static double runSimulation(Player player) {
         int score = 0;
         for (int i = 0; i < NUM_GAMES; i++) {
--- Project02/SuitStacker.java
+++ Project02/SuitStacker.java
@@ -1,3 +1,9 @@
+// CSCI 1913 - Project 2
+// @author: Ashwin Kalyan
+
+/**
+ * This AI attempts to make columns of cards with the same suit.
+ */
 public class SuitStacker implements Player {
     @Override
     public int nextMove(Card card, Board board) {
